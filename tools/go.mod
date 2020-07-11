e6ede4d0720d0161a9bf709d32491c5e24016698
Koichi Shiraishi <zchee.io@gmail.com>
2020-07-11 17:33:18 +0900

    tools: add tools

---
+++ tools/go.mod
@@ -0,0 +1,11 @@
+module go.lsp.dev/proto/tools
+
+go 1.14
+
+require (
+	github.com/googleapis/api-linter v1.1.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.0.0-beta.3
+	github.com/pseudomuto/protoc-gen-doc v1.3.2
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v0.0.0-20200707005602-4258d12073b4
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+)
---
+++ tools/tools.go
@@ -0,0 +1,15 @@
+// Copyright 2020 The Go Language Server Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+// +build tools
+
+// Package tools manages the project tools dependenncies.
+package tools
+
+import (
+	_ "github.com/googleapis/api-linter/cmd/api-linter"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+)
