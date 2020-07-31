073387a7eaad483c832fd83ad26973951c9d425f
Koichi Shiraishi <zchee.io@gmail.com>
2020-07-31 19:40:17 +0900

    tools: add tools

---
+++ tools/go.mod
@@ -0,0 +1,5 @@
+module go.lsp.dev/api/tools
+
+go 1.14
+
+require mvdan.cc/gofumpt v0.0.0-20200709182408-4fd085cb6d5f
---
+++ tools/tools.go
@@ -0,0 +1,11 @@
+// Copyright 2020 The Go Language Server Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+// +build tools
+
+// Package tools manages the project tools dependenncies.
+package tools
+
+import (
+	_ "mvdan.cc/gofumpt/gofumports"
+)
